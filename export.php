53f0b60c6aeac5f535b31a17a6af3eabf21f89d5
Ibnu Maksum <ibnumaksum@gmail.com>
2020-12-15 23:51:18 +0700

    add more link and demos

--- export.php
+++ export.php
@@ -1,40 +1,25 @@
 <?php
-$files = ['nsz', 'xci', 'updates', 'dlc'];
-$gmss = array();
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require '../config.php';
+require '../function.php';
+$files = ['nsz', 'xci', 'updates', 'dlc','demos'];
 foreach ($files as $file) {
-    $games = file_get_contents("$file/$file.txt");
-    $games = explode("\n", str_replace("\r", "", $games));
+    echo "$file\n";
+    if(file_exists($file . '/index.html')) unlink($file . '/index.html');
+    $games = $db->select('t_games',['id', 'title', 'fileSize'],['folder'=>$file,'ORDER'=>['title'=>'ASC']]);
     $html = '<!DOCTYPE html><html>
 <head><title>Index of ' . $file . '</title><meta charset="utf-8"></head>
 <body bgcolor="white">
 <h1>Index ' . $file . '</h1><hr><pre><a href="../">../</a>'."\n";
+    file_put_contents($file . '/index.html', $html,FILE_APPEND);
     foreach ($games as $game) {
-        if(checkFile($game)){
-            $gms = explode("#", $game);
-            $html .= '<a href="' . $game . '">' . $gms[1] . '</a>' . "\n";
+        echo $game['title']."\n";
+        if(!empty($game['title'])){
+            $html = '<a href="https://docs.google.com/uc?export=download&id=' . $game['id'] . '#' . urlencode(str_replace('#','',$game['title'])) . '">' . str_replace('#','',$game['title'])  . '</a>' . "\n";
+            file_put_contents($file . '/index.html', $html,FILE_APPEND);
         }
     }
-    $gmss = array_merge($gmss, $games);
-    $html .= '</pre><hr></body></html>';
-    file_put_contents($file . '/index.html', $html);
-    file_put_contents("$file/index.tfl", json_encode(['url' => $games, 'success' => 'Jangan di Abuse, beberapa game mungkin overload di download']));
-}
-file_put_contents("index.tfl", json_encode(['directories' => ['nsp', 'dlc', 'updates', 'xci'], 'success' => 'Jangan di Abuse, beberapa game mungkin overload di download']));
-
-
-function checkFile($url){
-    echo $url."\n";
-    $hasil = get_headers($url);
-    if(strpos($hasil[0],"200 OK")!==false){
-        echo "OK\n";
-        return ['OK'=>$hasil];
-    }elseif($hasil[0]=='HTTP/1.0 302 Moved Temporarily'){
-        foreach($hasil as $h){
-            if(strpos($h,"Location:")!==false){
-                return checkFile(substr($h,10,strlen($h)-10));
-            }
-        }
-    }
-    echo "NOT OK\n";
-    return false;
+    $html = '</pre><hr></body></html>';
+    file_put_contents($file . '/index.html', $html,FILE_APPEND);
 }
